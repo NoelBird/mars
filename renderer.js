b0b2966b88098d8f01f7d0c9a27ad9427fe52e8c
noelbird <lduldu00228@gmail.com>
2021-12-23 18:38:01 +0900

    render markdown

--- renderer.js
+++ renderer.js
@@ -4,3 +4,9 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+// import { marked } from 'marked';
+// // or const { marked } = require('marked');
+
+// const html = marked.parse('# Marked in Node.js\n\nRendered by **marked**.');
+// let content = document.getElementById("content")
+// content.innerHTML = html
